936cc37bada9c1fe89b39103caef1552b9f12dbc
Andrew Chen(ChungAn Chen) <106938666+andrewchen0511@users.noreply.github.com>
2023-03-24 19:32:01 +0800

    add ThreadPoolExecutor

--- healthcheck.py
+++ healthcheck.py
@@ -1,77 +1,70 @@
 #!/usr/bin/python
 import datetime
-import requests
 import os
-
-# All of healthcheck API欲監控之api及其正常的statuscode
-apilist = [
-    {'website':"https://...../api/token/", 'statuscode':400, "method":requests.post, "data":{"username": "","password": ""}},
-    {'website':"https://...../health", 'statuscode':200, "method":requests.get, "data":{}},
-    {'website':"https://...../staging/.../health", 'statuscode':200, "method":requests.get, "data":{}}
+import requests
+from typing import Dict
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-]
-# standard time def
-def time():
-    """
-        print the current time UTC +0:00
-    """
+# Constants
+CHAT_ID = "-1001716641802"  # WebHealthCheck-notification-popoint Chat Group ID 
+LINE_TOKEN = os.getenv('lineToken')  # K8s Secret env Inject # Line Notify API Token
+TELEGRAM_Bot_TOKEN = os.getenv('apiToken')  # PoService-AlarmBot's apiToken
+API_HEALTH_CONFIG = {
+    "https://website": {
+        "status_code": 400,
+        "method": requests.post,
+        "data": {"username": "", "password": ""},
+    },
+    "https://website": {"status_code": 200, "method": requests.get, "data": {},},
+}
+
+# Get standard time in the format of 'Y-m-d H:M:S (UTC +0:00)'
+def get_standard_time() -> str:
     try:
-        # Getting the standard time upto seconds only.
-        global Standard_time
-        Standard_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(f'Standard Time:{Standard_time}(UTC +0:00)')
-    except:
-        print('Something Wrong in main function',exc_info=True)
-
-if __name__=='__main__':
-    time()
-
-# Send-message-to-Line-Notify Function
-def send_to_line(message):
-    """
-        Send-message-to-Line-Notify Function
-    :param message: str type, the message what you want to send to Line Notify.
-    """
-    lineToken = os.getenv('lineToken')  # K8s Secert env Inject # Line Notify API Token
-    headers = { "Authorization": "Bearer " + lineToken }    # HTTP Head parameter & data
-    requests.post("https://notify-api.line.me/api/notify", headers = headers, data = { 'message':message })
-
-
-
-def send_to_telegram(message):
-    """
-        Send-message-to-telegram Function
-    :param message: str type, the message what you want to send to Telegram chat room.
-    """
-    
-    chatID = '<Add your ID>'    # WebHealthCheck-notification-popoint Chat Group ID : <Add your ID>
-    # Check ID website https://api.telegram.org/bot5834396372:AAFX3-TbwkK6Mhf8qfMANi-lOJAeGADyYgo/getUpdates
-    apiToken = os.getenv('apiToken')    # Service-AlarmBot's apiToken
-    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
-    try:
-        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
-        print(response.text)
+        return f'Standard Time:{datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")} (UTC +0:00)'
     except Exception as e:
-        print(e)
-
-
-def api_healthcheck(api_website, statuscode, method, data):
-    """
-        API status code healthcheck function
-    :param api_website: str type, to check api website.
-    :param statuscode: int type, the healthy status code of API.
-    :param method: requests.get, requests.post, requests.put ... various kinds requests you want to use.
-    :para data: dict type, HTTP POST data.
-    """
-    r = method(url=api_website,data = data)
-    print(api_website, r.status_code)
-    if r.status_code != int(statuscode):
-        # Line Alarm Messenge
-        send_to_line(f'ALARM!!! \nStandard Time:{Standard_time}(UTC +8:00) \nin Production.{str(api_website)}\nThis API is disconnect. StatusCode is {str(r.status_code)} \nPlease Check APISIX Status\n')
-        # telegram Alarm Messenge
-        send_to_telegram(f'ALARM!!! \nStandard Time:{Standard_time}(UTC +8:00) \nin Production.{str(api_website)}\nThis API is disconnect. StatusCode is {str(r.status_code)} \nPlease Check APISIX Status\n')
-        print(str(api_website) + '\n This API is disconnect. ' + 'StatusCode is ' + str(r.status_code))
+        print('Something Wrong in get_standard_time function', exc_info=True)
+        
+# Send message to Line Notify
+def send_to_line(message: str):
+    headers = {"Authorization": f"Bearer {LINE_TOKEN}"}
+    requests.post("https://notify-api.line.me/api/notify", headers=headers, data={"message": message})
     
-for i in range(0,len(apilist)):
-    api_healthcheck(apilist[i]['website'], apilist[i]['statuscode'], apilist[i]['method'], apilist[i]['data'])
+# Send message to Telegram
+def send_to_telegram(message: str):
+    chat_id = CHAT_ID
+    api_url = f"https://api.telegram.org/bot{TELEGRAM_Bot_TOKEN}/sendMessage"
+    try:
+        response = requests.post(api_url, json={"chat_id": chat_id, "text": message})
+        response.raise_for_status()
+        # print(response.text)
+    except requests.exceptions.HTTPError as e:
+        print(f"Error sending message to Telegram: {e}")
+    except Exception as e:
+        print(f"Unexpected error sending message to Telegram: {e}")
+
+def check_api_health(api_website: str, status_code: int, method: object, data: Dict):
+    r = method(url=api_website, data=data, timeout=(5, 10))
+    print(api_website, r.status_code)
+    if r.status_code != status_code:
+        # Line and Telegram Alarm Messages
+        std_time = get_standard_time()
+        message = (
+            f"ALARM!!!\nStandard Time: {std_time} (UTC +8:00)\n"
+            f"in Production. {api_website}\nThis API is disconnected. StatusCode is {r.status_code}\n"
+            "Please Check APISIX Status\n"
+        )
+        send_to_line(message)
+        send_to_telegram(message)
+        print(f"{api_website}\nThis API is disconnected. StatusCode is {r.status_code}")
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor(max_workers=len(API_HEALTH_CONFIG)) as executor:
+        futures = []
+        for api, config in API_HEALTH_CONFIG.items():
+            future = executor.submit(check_api_health, api, config["status_code"], config["method"], config["data"])
+            futures.append(future)
+        
+        for future in as_completed(futures):
+            result = future.result()
